2e2afdebfe7cb1a01a7c877d62d9df0ca847c4d5
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2024-09-24 17:39:48 +0200

    (feat) add Russian language (machine translated for now

--- apps/web/src/intl.config.ts
+++ apps/web/src/intl.config.ts
@@ -1,4 +1,4 @@
-export const SUPPORTED_LOCALES = ['en', 'de']
+export const SUPPORTED_LOCALES = ['en', 'de', 'ru']
 export const isSupportedLocale = (locale: string | '') =>
   SUPPORTED_LOCALES.includes(locale)
 export const DEFAULT_LOCALE = 'en'
