854c6e6dcbe6cc9fce33632749496d853859a11b
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2024-09-29 23:59:42 +0200

    (feat) add cascade delete for associated images when a user is deleted

--- apps/api/src/users/users.module.ts
+++ apps/api/src/users/users.module.ts
@@ -1,5 +1,9 @@
 import { ApiKeyService } from '@api/api_key/api_key.service'
 import { API_Key, API_KeySchema } from '@api/api_key/schemas/api_key.schema'
+import {
+  GeneratedImage,
+  GeneratedImageSchema,
+} from '@api/generated_image/schemas/generated_image.schema'
 import { Tier, TierSchema } from '@api/tier/schemas/tier.schema'
 import { User, UserSchema } from '@api/users/schemas/user.schema'
 import { Module } from '@nestjs/common'
@@ -7,13 +11,13 @@ import { MongooseModule } from '@nestjs/mongoose'
 import { UsersService } from './users.service'
 
 @Module({
-  // this way "UsersService can be used in other modules that import UsersModule"
   exports: [UsersService],
   imports: [
     MongooseModule.forFeature([
       { name: User.name, schema: UserSchema },
       { name: Tier.name, schema: TierSchema },
       { name: API_Key.name, schema: API_KeySchema },
+      { name: GeneratedImage.name, schema: GeneratedImageSchema },
     ]),
   ],
   providers: [ApiKeyService, UsersService],
